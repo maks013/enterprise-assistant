ee1b5ab019d56e42e05f277310a3b3cc08ab0d2a
Maksymilian Ulanecki <maks.u@vp.pl>
2023-12-31 00:24:40 +0100

    added products table initialization

---
+++ src/main/resources/db/migration/V3__init_products_table.sql
@@ -0,0 +1,9 @@
+create table products (
+    id                     serial primary key,
+    name                   varchar(255)   not null,
+    gtin                   varchar(255)   not null,
+    price_gross            numeric(19, 2) not null,
+    price_net              numeric(19, 2) not null,
+    image_url              varchar(255),
+    additional_information varchar(255)
+);
